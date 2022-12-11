ec026f6c371c480ce99b4ba223cf6246f8039885
Ebru Ünlü <ebruunlu3@gmail.com>
2022-12-11 21:04:51 +0300

    4.ders odevi

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace Koleksiyonlar
 {
     internal class Program
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string> dictionary = new Dictionary<string>();
+            dictionary.Add("table");
+            dictionary.Add("desk");
+
+            foreach (var item in dictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
