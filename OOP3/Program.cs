6158ff76b109023c75a32e0716d60bacc3e1ebd9
Ebru Ünlü <ebruunlu3@gmail.com>
2022-12-13 16:28:15 +0300

    5.ders sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+          
+            IKrediManager tasitKrediManager= new TasitKrediManager();
+    
+            IKrediManager konutKrediManager= new KonutKrediManager();
+
+            ILoggerService databaseLoggerService= new DatabaseLoggerService();
+            ILoggerService fileLogerService= new FileLoggerService();
+
+            BasvuruManager basvuruManager= new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, databaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+            
+        }
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i>= 0; i--)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine("çift sayı = "+ i);
+                }
+                else if (i%2==1) 
+                {
+                    Console.WriteLine("tek sayı = "+ i);
+                }
+                else {
+                    Console.WriteLine("sayı =" +i);
+                }
+            }
+
+        }
+    }
+}
